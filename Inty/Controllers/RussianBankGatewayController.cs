7eb5e771831c03e7a7d2fa993f0b7eef03b9a73f
Danila <danila351960@yandex.ru>
2025-03-25 15:06:31 +0300

    endpoint description added

--- Inty/Controllers/RussianBankGatewayController.cs
+++ Inty/Controllers/RussianBankGatewayController.cs
@@ -17,6 +17,13 @@ namespace Inty.Controllers
             _valuteCursInfoService = valuteCursInfoService;
         }
 
+        /// <summary>
+        /// Получить информацию о курсе валют
+        /// </summary>
+        /// <param name="dateTime">дата курса</param>
+        /// <param name="codes">код валюты (CH)</param>
+        /// <param name="cancellationToken">токен отмены операции</param>
+        /// <returns>список валют</returns>
         [HttpGet("curses")]
         public async Task<IActionResult> GetCurs([FromQuery] DateTime? dateTime, [FromQuery] string[] codes, CancellationToken cancellationToken = default)
         {
